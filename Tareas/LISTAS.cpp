bff38c192b7ca128d39d0df013c0de2cfb515346
Fernando Peña <Fernando@iMac-de-Familia.local>
2018-08-20 21:21:38 -0500

    Método para eliminar implementado.
    Se probó con casos base y casos extremos.
    Se añadió un método para saber si la lista está vacía.Método para eliminar implementado.
    
    Se probó con casos base y casos extremos.
    Se añadió un método para saber si la lista está vacía.

--- Tareas/LISTAS.cpp
+++ Tareas/LISTAS.cpp
@@ -11,15 +11,19 @@ typedef struct node {
 	struct node *ptr;
 } LISTA;
 
-LISTA *init_list, *aux, *e;
+LISTA *init_list, *aux, *aux2, *e;
 int Elems = 0;
+
+bool estaVacia(){
+	return init_list == NULL;
+}
 
 void agregaOrdenado(int data){
 	e = (LISTA*) malloc(sizeof(LISTA));
 	e -> n = data;
 	e -> ptr = NULL;
 
-	if(init_list == NULL || init_list -> n >= data){
+	if(estaVacia() || init_list -> n >= data){
 		e -> ptr = init_list;
 		init_list = e;
 	} else {
@@ -33,12 +37,38 @@ void agregaOrdenado(int data){
 	Elems++;
 }
 
+void elimina(int data){
+	if(estaVacia()) {
+		cout << "Lista vacía" << endl;
+	} else {
+		aux = init_list;
+
+		if(aux -> n == data){
+			init_list = init_list -> ptr;
+			free(aux);
+			Elems--;
+		} else {
+			while(aux != NULL && aux -> n != data){
+				aux2 = aux;
+				aux = aux -> ptr;
+			}
+			if(aux != NULL){
+				aux2 -> ptr = aux -> ptr;
+				free(aux);
+				Elems--;
+			} else {
+				cout << "El elemento no está en la lista." << endl;
+			}
+		}
+	}
+}
+
 void agregaFinal(int data){
 	e = (LISTA*) malloc(sizeof(LISTA));
 	e -> n = data;
 	e -> ptr = NULL;
 
-	if(init_list == NULL){
+	if(estaVacia()){
 		init_list = e;
 	} else {
 		aux = init_list;
@@ -51,8 +81,8 @@ void agregaFinal(int data){
 	Elems++;
 }
 
-void print(){
-	aux = init_list;
+void print(struct node *inicio){
+	aux = inicio;
 
 	while(aux != NULL){
 		cout << aux -> n << endl;
@@ -63,21 +93,22 @@ void print(){
 
 int main() {
 	int num;
+	elimina(10);
 	for (int i = 0; i < 5; i++){
-		cout << "Elemento " << (Elems + 1) << "  ";
-		cin >> num;
-		agregaFinal(num);
-	}
-
-	cout << "Los valores son: \n";
-	print();
-
-	for (int i = 0; i < 5; i++){
-		cout << "Elemento " << (Elems + 1) << "  ";
+		cout << "Agrega " << i + 1 << "  ";
 		cin >> num;
 		agregaOrdenado(num);
-		cout << "Los valores son: \n";
-		print();
+	}
+
+	cout << Elems << " elementos en la lista. Los valores son:" << endl;
+	print(init_list);
+
+	for (int i = 0; i < 5; i++){
+		cout << "Elimina " << i + 1 << "  ";
+		cin >> num;
+		elimina(num);
+		cout << Elems << " elementos en la lista. Los valores son:" << endl;
+		print(init_list);
 	}
 
 	cin.get();
