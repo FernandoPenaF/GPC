3ce55e7484deac18c1a9f7ea8b4d596f3d7c2cb5
LUIS FERNANDO PEÑA FLORES <lpeaflo1@itam.mx>
2018-09-03 14:33:05 -0500

    Ahora pinta dos cuadrados. :)

--- Clase/Prueba/main.cpp
+++ Clase/Prueba/main.cpp
@@ -6,15 +6,33 @@ void init(void) {
 	gluOrtho2D(0.0,200.0,0.0,150.0);
 }
 
-void lineSegment(void) {
+void cuadrado(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(5,0,0);
-	glBegin(GL_LINE_LOOP);
-		glVertex3f(10,15,-1);
-		glVertex3f(150,145,-1);
-		glColor3f(0, 5, 0);
-		glVertex3f(150, 15, -1);
-		glVertex3f(10, 145, -1);
+	glBegin(GL_QUADS);
+		glVertex3f(10, 10, -1);
+		glVertex3f(10, 20, -1);
+		glVertex3f(20, 20, -1);
+		glVertex3f(20, 10, -1);
+
+		glVertex3f(25, 10, -1);
+		glVertex3f(25, 20, -1);
+		glVertex3f(35, 20, -1);
+		glVertex3f(35, 10, -1);
+	glEnd();
+	glFlush();
+}
+
+void cuadrados(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(5, 0, 0);
+	glBegin(GL_QUADS);
+	for (int i = 0; i < 250; i = i + 25) {
+		glVertex3f(0, 0, -1);
+		glVertex3f(0, 10, -1);
+		glVertex3f(10, 10, -1);
+		glVertex3f(10, 0, -1);
+	}
 	glEnd();
 	glFlush();
 }
@@ -23,10 +41,10 @@ void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowPosition(200,0);
-	glutInitWindowSize(600,300);
+	glutInitWindowSize(450,400);
 	glutCreateWindow("Ejemplo");
 	init();
-	glutDisplayFunc(lineSegment);
+	glutDisplayFunc(cuadrado);
 	glutMainLoop();
 }
 	
