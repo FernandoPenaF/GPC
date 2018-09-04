38fc6059de40de498c9aa7038235787070bedbc3
LUIS FERNANDO PEÑA FLORES <lpeaflo1@itam.mx>
2018-09-04 14:14:24 -0500

    Método más o menos general para hacer un cuadrado.
    Hace un cuadrado con borde hecho con pequeños cuadrados

--- Clase/Prueba/main.cpp
+++ Clase/Prueba/main.cpp
@@ -7,16 +7,43 @@ void init(void) {
 }
 
 void cuadrado(void) {
-	int offset = 4;
+	double xOffset = 4.2;
+	double yOffset = 4.4;
+	int xInit = 5;
+	int yInit = 5;
+	int xLimit = 145;
+	int yLimit = 190;
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0,0,0);
+	glColor3f(0.3,0.3,0.3);
 	glBegin(GL_QUADS);
-		for (int i = 10; i < 140; i = i + 5) {
-			glVertex3f(10, i, -1);
-			glVertex3f(10, i + offset, -1);
-			glVertex3f(15, i + offset, -1);
-			glVertex3f(15, i, -1);
+		for (int i = xInit; i < xLimit; i = i + 5) {
+			glVertex3f(xInit, i, -1);
+			glVertex3f(xInit, i + yOffset, -1);
+			glVertex3f(xInit + xOffset, i + yOffset, -1);
+			glVertex3f(xInit + xOffset, i, -1);
 		}
+
+		for (int i = yInit; i < yLimit; i = i + 5) {
+			glVertex3f(i + xOffset, yInit, -1);
+			glVertex3f(i + xOffset, yInit + yOffset, -1);
+			glVertex3f(i, yInit + yOffset, -1);
+			glVertex3f(i, yInit, -1);
+		}
+
+		for (int i = xInit; i < xLimit + 5; i = i + 5) {
+			glVertex3f(yLimit, i, -1);
+			glVertex3f(yLimit, i + yOffset, -1);
+			glVertex3f(yLimit + xOffset, i + yOffset, -1);
+			glVertex3f(yLimit + xOffset, i, -1);
+		}
+
+		for (int i = yInit; i < yLimit; i = i + 5) {
+			glVertex3f(i + xOffset, xLimit, -1);
+			glVertex3f(i + xOffset, xLimit + yOffset, -1);
+			glVertex3f(i, xLimit + yOffset, -1);
+			glVertex3f(i, xLimit, -1);
+		}
+
 		
 	glEnd();
 	glFlush();
@@ -40,7 +67,7 @@ void main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowPosition(200,0);
-	glutInitWindowSize(750,600);
+	glutInitWindowSize(600,600);
 	glutCreateWindow("Ejemplo");
 	init();
 	glutDisplayFunc(cuadrado);
