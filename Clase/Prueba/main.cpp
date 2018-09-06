af2b18b8c0282da075f1682e6763715433b37ac6
LUIS FERNANDO PEÑA FLORES <lpenaf@itam.mx>
2018-09-06 16:10:13 -0500

    Cambios en los colores.

--- Clase/Prueba/main.cpp
+++ Clase/Prueba/main.cpp
@@ -1,7 +1,7 @@
 #include "GL/glut.h"
 
 void init(void) {
-	glClearColor(1.0,1.0,1.0,0.0);
+	glClearColor(0.5803,0.0,0.827,0.0);
 	glMatrixMode(GL_PROJECTION);
 	gluOrtho2D(0.0,200.0,0.0,150.0);
 }
@@ -9,13 +9,13 @@ void init(void) {
 void cuadrado(void) {
 	double xOffset = 4.2;
 	double yOffset = 4.4;
-	int xInit = 5;
-	int yInit = 5;
+	int xInit = 0;
+	int yInit = 0;
 	int xLimit = 145;
-	int yLimit = 190;
+	int yLimit = 195;
 	int cont = 0;
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0.3,0.3,0.3);
+	glColor3f(1.0,0.647,0.0);
 	glBegin(GL_QUADS);
 	while (cont < 2) {
 		for (int i = xInit; i < xLimit; i = i + 5) {
@@ -50,6 +50,11 @@ void cuadrado(void) {
 		yLimit -= 5;
 		cont++;
 	}
+
+	glVertex3f(15, 40, -1);
+	glVertex3f(70, 175, -1);
+	glVertex3f(135, 40, -1);
+	glVertex3f(70, 15,-1);
 		
 	glEnd();
 	glFlush();
