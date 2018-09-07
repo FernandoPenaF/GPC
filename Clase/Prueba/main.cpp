428c506e0ffd05e627205679389bbe7753212954
LUIS FERNANDO PEÑA FLORES <lpeaflo1@itam.mx>
2018-09-07 17:56:18 -0500

    Se pintan más triangulos.
    Se cambian los colores.

--- Clase/Prueba/main.cpp
+++ Clase/Prueba/main.cpp
@@ -1,12 +1,13 @@
 #include "GL/glut.h"
 
 void init(void) {
-	glClearColor(0.5803,0.0,0.827,0.0);
+	glClearColor(0.980, 0.922, 0.843,0.0);
 	glMatrixMode(GL_PROJECTION);
 	gluOrtho2D(0.0,200.0,0.0,150.0);
 }
 
 void cuadrado(void) {
+	double colorIncrement, colorCode;
 	double xOffset = 4.2;
 	double yOffset = 4.4;
 	int increment = 5;
@@ -16,7 +17,7 @@ void cuadrado(void) {
 	int yLimit = 195;
 	int cont = 0;
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0,0.647,0.0);
+	glColor3f(0.15, 0.15, 0.15);
 	glBegin(GL_QUADS);
 	while (cont < 2) {
 		for (int i = xInit; i < xLimit; i = i + increment) {
@@ -45,6 +46,7 @@ void cuadrado(void) {
 			glVertex3f(i, xLimit + yOffset, -1);
 			glVertex3f(i, xLimit, -1);
 		}
+		glColor3f(0.25, 0.25, 0.25);
 		xInit += 5;
 		yInit += 5;
 		xLimit -= 5;
@@ -54,15 +56,22 @@ void cuadrado(void) {
 	glEnd();
 	
 	glBegin(GL_TRIANGLES);
-		glColor3f(1.0,0.647,0.0);
+	colorIncrement = 0.045;
+	colorCode = 0.35;
+		for (int i = 0; i < 20; i++) {
+			glColor3f(colorCode + i * colorIncrement, colorCode + i * colorIncrement, colorCode + i * colorIncrement);
+			glVertex3f(xInit + i * increment, (yLimit - yInit - 5 * increment) / 2, -1);
+			glVertex3f(xLimit - xInit - 6 * increment, yLimit - yInit - 7 * increment, -1);
+			glVertex3f(xLimit - xInit - 6 * increment, yInit, -1);
+		}
 
-		glVertex3f(xInit, (yLimit - yInit - 5 * increment) / 2, -1);
-		glVertex3f(xLimit - xInit - 6 * increment, yLimit - yInit - 7 * increment, -1);
-		glVertex3f(xLimit - xInit - 6 * increment, yInit, -1);
+		for (int i = 0; i < 20; i++) {
+			glColor3f(colorCode + i * colorIncrement, colorCode + i * colorIncrement, colorCode + i * colorIncrement);
+			glVertex3f(xLimit - xInit - 4 * increment, yInit, -1);
+			glVertex3f(xLimit - xInit - 4 * increment, yLimit - yInit - 7 * increment, -1);
+			glVertex3f(xLimit + 11 * increment - i * increment, (yLimit - yInit - 5 * increment) / 2, -1);
+		}
 
-		glVertex3f(xLimit - xInit - 4 * increment, yInit, -1);
-		glVertex3f(xLimit - xInit - 4 * increment, yLimit - yInit - 7 * increment, -1);
-		glVertex3f(xLimit + 11 * increment, (yLimit - yInit - 5 * increment) / 2, -1);
 	glEnd();
 
 	glFlush();
