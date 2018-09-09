a8885046f315cb783d12606ee3e376780acfa2f9
lpenaf <lpeaflo1@itam.mx>
2018-09-09 13:02:10 -0500

    Se pintan triángulos externos.

--- Clase/Prueba/main.cpp
+++ Clase/Prueba/main.cpp
@@ -15,77 +15,90 @@ void cuadrado(void) {
 	int yInit = 0;
 	int xLimit = 145;
 	int yLimit = 195;
+	int tempXInit, tempYInit, tempXLimit, tempYLimit;
 	int cont = 0;
+
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0.15, 0.15, 0.15);
+	glColor3f(0.25, 0.25, 0.25);
+
 	glBegin(GL_QUADS);
-	while (cont < 2) {
-		for (int i = xInit; i < xLimit; i = i + increment) {
-			glVertex3f(xInit, i, -1);
-			glVertex3f(xInit, i + yOffset, -1);
-			glVertex3f(xInit + xOffset, i + yOffset, -1);
-			glVertex3f(xInit + xOffset, i, -1);
-		}
-		for (int i = yInit; i < yLimit; i = i + increment) {
-			glVertex3f(i + xOffset, yInit, -1);
-			glVertex3f(i + xOffset, yInit + yOffset, -1);
-			glVertex3f(i, yInit + yOffset, -1);
-			glVertex3f(i, yInit, -1);
-		}
+		while (cont < 2) {
+			for (int i = xInit; i < xLimit; i = i + increment) {
+				glVertex3f(xInit, i, -1);
+				glVertex3f(xInit, i + yOffset, -1);
+				glVertex3f(xInit + xOffset, i + yOffset, -1);
+				glVertex3f(xInit + xOffset, i, -1);
+			}
 
-		for (int i = xInit; i < xLimit + increment; i = i + increment) {
-			glVertex3f(yLimit, i, -1);
-			glVertex3f(yLimit, i + yOffset, -1);
-			glVertex3f(yLimit + xOffset, i + yOffset, -1);
-			glVertex3f(yLimit + xOffset, i, -1);
-		}
+			for (int i = yInit; i < yLimit; i = i + increment) {
+				glVertex3f(i + xOffset, yInit, -1);
+				glVertex3f(i + xOffset, yInit + yOffset, -1);
+				glVertex3f(i, yInit + yOffset, -1);
+				glVertex3f(i, yInit, -1);
+			}
 
-		for (int i = yInit; i < yLimit; i = i + increment) {
-			glVertex3f(i + xOffset, xLimit, -1);
-			glVertex3f(i + xOffset, xLimit + yOffset, -1);
-			glVertex3f(i, xLimit + yOffset, -1);
-			glVertex3f(i, xLimit, -1);
+			for (int i = xInit; i < xLimit + increment; i = i + increment) {
+				glVertex3f(yLimit, i, -1);
+				glVertex3f(yLimit, i + yOffset, -1);
+				glVertex3f(yLimit + xOffset, i + yOffset, -1);
+				glVertex3f(yLimit + xOffset, i, -1);
+			}
+
+			for (int i = yInit; i < yLimit; i = i + increment) {
+				glVertex3f(i + xOffset, xLimit, -1);
+				glVertex3f(i + xOffset, xLimit + yOffset, -1);
+				glVertex3f(i, xLimit + yOffset, -1);
+				glVertex3f(i, xLimit, -1);
+			}
+
+			glColor3f(0.35, 0.35, 0.35);
+			xInit += 5;
+			yInit += 5;
+			xLimit -= 5;
+			yLimit -= 5;
+			cont++;
 		}
-		glColor3f(0.25, 0.25, 0.25);
-		xInit += 5;
-		yInit += 5;
-		xLimit -= 5;
-		yLimit -= 5;
-		cont++;
-	}
 	glEnd();
 	
+	tempXInit = xInit;
+	tempYInit = yInit;
+	tempXLimit = xLimit;
+	tempYLimit = yLimit;
+
 	glBegin(GL_TRIANGLES);
-	colorIncrement = 0.045;
-	colorCode = 0.35;
-		for (int i = 0; i < 20; i++) {
-			glColor3f(colorCode + i * colorIncrement, colorCode + i * colorIncrement, colorCode + i * colorIncrement);
-			glVertex3f(xInit + i * increment, (yLimit - yInit - 5 * increment) / 2, -1);
-			glVertex3f(xLimit - xInit - 6 * increment, yLimit - yInit - 7 * increment, -1);
-			glVertex3f(xLimit - xInit - 6 * increment, yInit, -1);
-		}
+		colorIncrement = 0.045;
+		colorCode = 0.35;
+			for (int i = 0; i < 20; i++) {
+				glColor3f(colorCode + i * colorIncrement, colorCode + i * colorIncrement, colorCode + i * colorIncrement);
+				glVertex3f(xInit + i * increment, (yLimit - yInit - 5 * increment) / 2, -1);
+				glVertex3f(xLimit - xInit - 6 * increment, yLimit - yInit - 7 * increment, -1);
+				glVertex3f(xLimit - xInit - 6 * increment, yInit, -1);
+			}
 
-		for (int i = 0; i < 20; i++) {
-			glColor3f(colorCode + i * colorIncrement, colorCode + i * colorIncrement, colorCode + i * colorIncrement);
-			glVertex3f(xLimit - xInit - 4 * increment, yInit, -1);
-			glVertex3f(xLimit - xInit - 4 * increment, yLimit - yInit - 7 * increment, -1);
-			glVertex3f(xLimit + 11 * increment - i * increment, (yLimit - yInit - 5 * increment) / 2, -1);
-		}
+			for (int i = 0; i < 20; i++) {
+				glColor3f(colorCode + i * colorIncrement, colorCode + i * colorIncrement, colorCode + i * colorIncrement);
+				glVertex3f(xLimit - xInit - 4 * increment, yInit, -1);
+				glVertex3f(xLimit - xInit - 4 * increment, yLimit - yInit - 7 * increment, -1);
+				glVertex3f(xLimit + 11 * increment - i * increment, (yLimit - yInit - 5 * increment) / 2, -1);
+			}
 	glEnd();
-	glFlush();
-}
+	
+	xInit = tempXInit;
+	yInit = tempYInit;
+	xLimit = tempXLimit;
+	yLimit = tempYLimit;
 
-void cuadrados(void) {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(5, 0, 0);
-	glBegin(GL_QUADS);
-	for (int i = 0; i < 250; i = i + 25) {
-		glVertex3f(0, 0, -1);
-		glVertex3f(0, 10, -1);
-		glVertex3f(10, 10, -1);
-		glVertex3f(10, 0, -1);
-	}
+	glColor3f(0, 1.0, 0);
+	glBegin(GL_TRIANGLES);
+		glVertex3f(xInit, (yLimit - yInit - 4.8 * increment) / 2, -1);
+		glVertex3f(xLimit - xInit - 6.2 * increment, yLimit - yInit - 7 * increment - yOffset / 8, -1);
+		glVertex3f(xInit, yLimit - yInit - 7 * increment - yOffset / 8, -1);
+
+		glVertex3f(xLimit - xInit - 3.8 * increment, yInit, -1);
+		glVertex3f(xLimit + 10.9 * increment, (yLimit - yInit - 5.4 * increment) / 2, -1);
+		glVertex3f(xLimit + 10.9 * increment, yInit, -1);
 	glEnd();
+
 	glFlush();
 }
 
